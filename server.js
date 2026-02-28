1bee998b66bf4f934550c1c8d98bb1726b814f37
Anshu sharma <156065826+Anshusharma42019@users.noreply.github.com>
2026-02-28 11:12:25 +0530

    vercel

--- server.js
+++ server.js
@@ -5,7 +5,23 @@ const cors = require('cors');
 
 const app = express();
 
-app.use(cors());
+const allowedOrigins = [
+  "http://localhost:5173",
+  ""
+];
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+    optionsSuccessStatus: 204,
+  })
+);
 app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URL)
